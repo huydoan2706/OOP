930926117a32e181b094a2f5a195732439189675
huydoan2706 <166465466+huydoan2706@users.noreply.github.com>
2024-05-19 15:07:54 +0700

    Delete HippocampusChess directory

--- HippocampusChess/HOME/Home.java
+++
@@ -1,9 +0,0 @@
-package HippocampusChess.HOME;
-
-import java.awt.*;
-
-abstract public class Home {
-    int Vertical_Width = 100;
-    int Horizontal_Width = 180;
-    abstract public void paint(Graphics2D g2d);
-}
--- HippocampusChess/PLACE/StandingPlace.java
+++
@@ -1,8 +0,0 @@
-package HippocampusChess.PLACE;
-
-import java.awt.*;
-
-abstract public class StandingPlace {
-    final int DIAMETER = 30;
-    abstract public void paint(Graphics2D g2d);
-}
